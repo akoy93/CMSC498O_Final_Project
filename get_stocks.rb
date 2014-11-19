bb2ae75339e326b6bde510b523de3599d10bc38b
Albert Koy <albertkoy@gmail.com>
2014-11-19 18:10:55 -0500

    finished script to compute technical indicators

--- get_stocks.rb
+++ get_stocks.rb
@@ -1,15 +1,15 @@
 require 'date'
 require 'open-uri'
 
-# usage ruby get_stocks.rb #{STOCK_SYMBOLS} #{NUMBER_OF_BARS}
+# usage ruby get_stocks.rb #{STOCK_SYMBOLS} #{NUMBER_OF_YEARS}
 
 unless ARGV.length == 2
-  puts "Incorrect Usage. Use: ruby get_stocks.rb {STOCK_SYMBOLS_FILE} {NUMBER_OF_BARS}"
+  puts "Incorrect Usage. Use: ruby get_stocks.rb {STOCK_SYMBOLS_FILE} {NUMBER_OF_YEARS}"
 else
   current_date = Date.parse(Time.now.to_s)
   filename = ARGV[0]
-  num_bars = ARGV[1].to_i
-  directory = "#{current_date.to_s}_#{num_bars}"
+  num_years = ARGV[1].to_i
+  directory = "#{current_date.to_s}_#{num_years}yr"
   symbols = []
 
   # remove existing directory
@@ -21,9 +21,8 @@ else
     sym.chomp!
     puts "Fetching #{sym}..."
     # api url for stock data retrieval
-    start_date = current_date - num_bars
     retrieval_url = "http://real-chart.finance.yahoo.com/table.csv?s=#{sym}" +
-      "&a=#{start_date.month - 1}&b=#{start_date.day}&c=#{start_date.year}&d=#{current_date.month - 1}" +
+      "&a=#{current_date.month - 1}&b=#{current_date.day}&c=#{current_date.year - num_years}&d=#{current_date.month - 1}" +
       "&e=#{current_date.day}&f=#{current_date.year}&g=d&ignore=.csv"
     begin
       open("#{directory}/#{sym}.csv", "wb") do |file|
